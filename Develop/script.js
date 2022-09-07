7444fc66c43d93651f409a814dd32ac4968d0ba6
Parija Parekh <parija.parekh@gmail.com>
2022-09-07 10:17:45 +1000

    With timer functionality. Score display pending

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,4 @@
-let numOfOptions = 4;
- 
-let questions = [
+const questions = [
 {
     "question": "What is coding",
     "options": [
@@ -46,9 +44,19 @@ let questions = [
         {id: '4', value: 'dasdas'}],
     "ans": "a"
 },]
+let userQuestions = [];
+const incorrectPenalty = 10; // lose 10secs
+const counter = 60;
+const groupByStr = "answer";
+const startButtonEl = document.querySelector(".start-button");
+const timerEl = document.querySelector("#timer")
+const questionEl = document.querySelector("#question"); 
+const feedbackEl = document.querySelector("#feedback");
+const submitButtonEl = document.createElement("button");
 
+
+// functions
 function renderOptions(parentEl, optionsList){
-    //console.log(optionsList)
     var a = optionsList.map((option) => {
         return `<div><input type="radio" name="answer" value="${option.value}" id="${option.id}">
         <label for="${option.id}">${option.value}</label>
@@ -56,20 +64,42 @@ function renderOptions(parentEl, optionsList){
     parentEl.innerHTML += a.join(' ');   
 }
 
-var submitButton = document.createElement("button");
-let userQuestions = [];
+function getCheckedValue(groupBy){
+    var options = document.getElementsByName(groupBy);
+    for(i = 0; i < options.length; i++){
+        if(options[i].checked){
+            return options[i].value;
+        }
+    }
+}
 
+function resetLocalStorage(counter){
+    userQuestions = [];
+    localStorage.setItem("userQuestions", JSON.stringify(userQuestions));
+    localStorage.setItem("timer", JSON.stringify(counter));   
+}
+
+function stopQuiz(){
+    questionEl.textContent = "";
+    feedbackEl.textContent = "";
+    timerEl.style.display = "none";
+    questionEl.style.display = "none";
+    feedbackEl.style.display = "none";
+    //displayScoreForm();
+}
 
 /* get a random index and populate the question div */
 function renderQuestion(documentPos){
     var repeated = true;
     var x;
     var i = 0;
+    let timer = JSON.parse(localStorage.getItem("timer"));
 
     if (localStorage.hasOwnProperty("userQuestions")){
         userQuestions = JSON.parse(localStorage.getItem("userQuestions"));
     }
-    if (userQuestions.length < questions.length){
+    console.log(timer);
+    if ((userQuestions.length < questions.length) && (timer > 0)){
         while (repeated){
             x = questions[Math.floor(Math.random()* questions.length)];
             if (userQuestions.length > 0){
@@ -92,41 +122,56 @@ function renderQuestion(documentPos){
         questionEl.textContent = x.question;  
         renderOptions(questionEl, x.options);
         // work on the placement of submit button along with its styling 
-        submitButton.textContent = "Submit";
-        questionEl.appendChild(submitButton)
+        submitButtonEl.textContent = "Submit";
+        questionEl.appendChild(submitButtonEl)
         documentPos.appendChild(questionEl);
         // The question needs to be stored on client side. 
         var len = userQuestions.unshift(x);
         localStorage.setItem("userQuestions", JSON.stringify(userQuestions));
-    }
-}
+    }// end of if loop
+    else{
+        stopQuiz();
+    } 
 
-
-function getCheckedValue(){
-    var options = document.getElementsByName('answer');
-    for(i = 0; i < options.length; i++){
-        if(options[i].checked){
-            return options[i].value;
-        }
-    }
 }
 
 function startCounter(timeLeft){
     var timeInterval = setInterval(function(){
+        if (localStorage.hasOwnProperty("timer")){
+            timeLeft = JSON.parse(localStorage.getItem("timer"));
+        }
+        console.log(timeLeft);
         if (timeLeft >= 0){
             timerEl.textContent  = timeLeft;    
             timeLeft-- ;
-      }
-      else {
-        clearInterval(timeInterval);
-        stopQuiz();
-      }
+        }
+        else {
+            clearInterval(timeInterval);
+            stopQuiz();
+        }
+        localStorage.setItem("timer", JSON.stringify(timeLeft));
     },1000);
  }
 
-submitButton.addEventListener("click", function(event){
+function alterTimer(intervalSkip, operator){
+    let timer = 0;
+    if (localStorage.hasOwnProperty("timer")){
+        timer = JSON.parse(localStorage.getItem("timer"));
+    } 
+    if (operator === "decrease"){
+        if (timer - intervalSkip >= 0){
+            timer -= intervalSkip;
+            localStorage.setItem("timer", JSON.stringify(timer));
+        }
+        else {
+            stopQuiz();
+        }
+     }
+}
+
+submitButtonEl.addEventListener("click", function(event){
     if (event.target.matches("button")){
-        let userAns = getCheckedValue();
+        let userAns = getCheckedValue(groupByStr);
         if (userAns){
             console.log("User Answer: " +userAns);
             console.log("Actual Answer: "+userQuestions[0].ans);
@@ -139,6 +184,7 @@ submitButton.addEventListener("click", function(event){
             }
             else{
                 feedbackEl.textContent += `Answer to ${userQuestions[0].question}:  Incorrect! \r\n`;
+                alterTimer(incorrectPenalty, "decrease");
             }
             questionEl.textContent=""; 
             renderQuestion(questionEl);
@@ -146,27 +192,16 @@ submitButton.addEventListener("click", function(event){
         else{
             window.alert("Select your answer to proceed in the quiz");
         }
-            //clearQuestionDiv();
-        //updateLastAnsDiv();
-        //if timer time left and all questions not done
-            //renderQuestion();
     }
 });
 
-function clearUserQuestions(){
-    userQuestions = [];
-    localStorage.setItem("userQuestions", JSON.stringify(userQuestions));    
-}
-
-var startButtonEl = document.querySelector(".start-button");
-var timerEl = document.querySelector("#timer")
-var questionEl = document.querySelector("#question"); 
-var feedbackEl = document.querySelector("#feedback");
-
 startButtonEl.addEventListener("click", function(event){
     if (event.target.matches("button")){
-        clearUserQuestions();
-        startCounter(60);
+        timerEl.style.display = "block";
+        questionEl.style.display = "block";
+        feedbackEl.style.display = "block";
+        resetLocalStorage(counter);
+        startCounter(counter);
         renderQuestion(questionEl);
     }    
 })
